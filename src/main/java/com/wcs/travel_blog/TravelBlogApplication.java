8bd76f7d42d4905be67c83fa9421077281acfca1
lpavius <lpavius@student.42.fr>
2025-04-17 17:16:26 +0200

    chore: architecture ok

--- src/main/java/com/wcs/travel_blog/TravelBlogApplication.java
+++ src/main/java/com/wcs/travel_blog/TravelBlogApplication.java
@@ -1,5 +1,6 @@
 package com.wcs.travel_blog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelBlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TravelBlogApplication.class, args);
-	}
 
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("CORS_ALLOWED_ORIGIN", dotenv.get("CORS_ALLOWED_ORIGIN"));
+
+		SpringApplication.run(TravelBlogApplication.class, args);
+
+	}
 }
