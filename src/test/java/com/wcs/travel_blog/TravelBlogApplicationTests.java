180bd037eb3de44c0286a11d9f6f197a74ddba81
lpavius <lpavius@student.42.fr>
2025-04-17 12:31:14 +0200

    Initial commit

---
+++ src/test/java/com/wcs/travel_blog/TravelBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.travel_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
