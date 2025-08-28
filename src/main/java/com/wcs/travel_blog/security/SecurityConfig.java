dcd8bd2e0a5fe4770d4b6f5ce613f3eed44cb75a
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-08-28 14:18:45 +0200

    feat(KAN-248):restore BCryptPasswordEncoder

--- src/main/java/com/wcs/travel_blog/security/SecurityConfig.java
+++ src/main/java/com/wcs/travel_blog/security/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -73,6 +73,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
