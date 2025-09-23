3f8c0f3f678d2f268bb1c8938470eace7abfea25
Hélène Rampelberg <h.rampelberg@yahoo.fr>
2025-09-23 23:26:35 +0200

    feat(KAN-255): JWT implementation with HttpOnly cookies

--- src/main/java/com/wcs/travel_blog/auth/controller/AuthController.java
+++ src/main/java/com/wcs/travel_blog/auth/controller/AuthController.java
@@ -5,20 +5,31 @@ import com.wcs.travel_blog.auth.dto.UserRegistrationDTO;
 import com.wcs.travel_blog.auth.service.AuthService;
 import com.wcs.travel_blog.user.dto.UserDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.Set;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
+
+    @Value("${jwt.cookie.secure}")
+    private boolean cookieSecure;
+
+    @Value("${jwt.cookie.samesite}")
+    private String sameSite;
 
     public AuthController(AuthService authService){
         this.authService = authService;
@@ -38,7 +49,30 @@ public class AuthController {
                 userLoginDTO.getEmail(),
                 userLoginDTO.getPassword()
         );
-        return ResponseEntity.ok(token);
+
+        ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                .httpOnly(true)
+                .secure(cookieSecure)
+                .path("/")
+                .maxAge(Duration.ofHours(1))
+                .sameSite(sameSite)
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Connexion réussie");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(){
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(cookieSecure)
+                .path("/")
+                .maxAge(0)
+                .build();
+        return ResponseEntity.noContent()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 
 
