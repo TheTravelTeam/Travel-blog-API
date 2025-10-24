073ea157cb7c94abb0c066bc9b24bc1bb3a4cef8
antop <antoine.peral@rossel.fr>
2025-10-24 16:28:03 +0200

    fix: fix release

--- src/main/java/com/wcs/travel_blog/article/service/ArticleService.java
+++ src/main/java/com/wcs/travel_blog/article/service/ArticleService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
