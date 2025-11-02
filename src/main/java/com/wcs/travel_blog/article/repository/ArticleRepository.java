c329cb0aa59a3c44c14b5a229ae0eb33c2bae429
Hélène Rampelberg <h.rampelberg@yahoo.fr>
2025-11-03 00:38:56 +0100

    feat(No-ticketÂ): add find by slug in article CRUD

--- src/main/java/com/wcs/travel_blog/article/repository/ArticleRepository.java
+++ src/main/java/com/wcs/travel_blog/article/repository/ArticleRepository.java
@@ -3,5 +3,8 @@ package com.wcs.travel_blog.article.repository;
 import com.wcs.travel_blog.article.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<Article,Long> {
+    Optional<Article> findBySlug(String slug);
 }
